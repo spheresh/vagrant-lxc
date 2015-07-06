fdcf94a040a754d1ecfbb166f1d34a466006cf73
Michael Stucki <michael@stucki.io>
2015-07-06 17:44:35 +0200

    Private networking: Create bridge if it is missing

--- lib/vagrant-lxc/driver.rb
+++ lib/vagrant-lxc/driver.rb
@@ -150,6 +150,20 @@ module Vagrant
         ]
         @sudo_wrapper.run(*cmd)
 
+        if ! bridge_exists?(bridge_name)
+          if not bridge_ip
+            raise "Bridge is missing and no IP was specified!"
+          end
+
+          @logger.info "Creating the bridge #{bridge_name}"
+          cmd = [
+            'brctl',
+            'addbr',
+            bridge_name
+          ]
+          @sudo_wrapper.run(*cmd)
+        end
+
         if ! bridge_has_an_ip?(bridge_name)
           @logger.info "Adding #{bridge_ip} to the bridge #{bridge_name}"
           cmd = [
@@ -161,6 +175,7 @@ module Vagrant
             bridge_name
           ]
           @sudo_wrapper.run(*cmd)
+          @sudo_wrapper.run('ifconfig', bridge_name, 'up')
         end
       end
 
